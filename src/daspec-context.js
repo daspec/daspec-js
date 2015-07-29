51918bb7c91f73ce17994b6559c6642a84855841
Gojko Adzic <gojko@gojko.com>
2015-07-29 11:53:31 +0200

    todo for non-capture-groups

--- src/daspec-context.js
+++ src/daspec-context.js
@@ -1,31 +1,4 @@
 /*global module, require*/
-
-/*
-move formating out of context
-
-
-- responsibilities
-
-	-> provide md source file/s
-		-> web loader
-		-> file loader
-		-> text area loader
-	-> convert between page md and blocks (parsing)						[ExampleBlocks]
-	-> convert between blocks and "steps"
-		-> allow users to define matchers for steps						[Context]
-		-> convert between blocks and "steps" (execution)				[Context]
-
-	-> execution/runner
-		-> control the flow on a single page (setup/steps/teardown/beforeeach/aftereach) [Runner]
-		-> control the flow on a whole suite (suite setup/pages/suite teardown/beforeall/afterall) [Runner]
-		-> execute "steps"												[Runner]
-		-> carry "step" execution info in a format-agnostic way			[Step]
-	-> format results													[Formatter]
-		-> format results as md
-		-> format results as counts (junit xml or tap)
-
-
-*/
 module.exports = function () {
 	'use strict';
 	var self = this,
@@ -33,6 +6,7 @@ module.exports = function () {
 		steps = [];
 
 	self.defineStep = function (regexMatcher, processFunction) {
+		/* TODO: bork if it has non capture groups */
 		steps.push(new StepExecutor(regexMatcher, processFunction));
 	};
 	self.getStepForLine = function (stepText) {
