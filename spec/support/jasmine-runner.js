68a0f838bbf626ba17dcd24bd42dbd82dc1f6a22
Gojko Adzic <gojko@gojko.com>
2015-08-09 03:29:02 +0200

    added a pretest jshint step

--- spec/support/jasmine-runner.js
+++ spec/support/jasmine-runner.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-var Jasmine = require('jasmine');
-var SpecReporter = require('jasmine-spec-reporter');
-var noop = function() {};
-
-var jrunner = new Jasmine();
+/*global jasmine*/
+var Jasmine = require('jasmine'),
+	SpecReporter = require('jasmine-spec-reporter'),
+	noop = function () {},
+	jrunner = new Jasmine();
 if (process.argv[2] === 'full') {
 	jrunner.configureDefaultReporter({print: noop});    // remove default reporter logs
 	jasmine.getEnv().addReporter(new SpecReporter());   // add jasmine-spec-reporter
