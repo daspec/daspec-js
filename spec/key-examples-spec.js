1a0ae582c32cdc96d3515c9667deb639c15444ec
Gojko Adzic <gojko@gojko.com>
2015-08-14 16:21:38 +0200

    value cleanup to int

--- spec/key-examples-spec.js
+++ spec/key-examples-spec.js
@@ -1,4 +1,4 @@
-/*global describe, expect, it, require, DaSpecHelper  */
+/*global describe, expect, it, require, DaSpecHelper, global  */
 
 describe('Key Examples from test-data', function () {
 	'use strict';
@@ -6,11 +6,20 @@ describe('Key Examples from test-data', function () {
 		Runner = require('../src/runner'),
 		MarkDownResultFormatter = require('../src/markdown-result-formatter'),
 		helper = new DaSpecHelper(),
-		exampleFiles = helper.getExamples();
+		exampleFiles = helper.getExamples(),
+		exportObject = function (ob) {
+			var c;
+			for (c in ob) {
+				global[c] = ob[c];
+			}
+		};
 	exampleFiles.forEach(function (exampleName) {
 		var example = helper.loadExample(exampleName);
 		it(example.title, function () {
-			var runner = new Runner(stepDefinitions),
+			var runner = new Runner(function (specContext) {
+					exportObject(specContext);
+					stepDefinitions(specContext);
+				}),
 				resultFormatter = new MarkDownResultFormatter(runner);
 			runner.execute(example.input);
 			expect(resultFormatter.formattedResults()).toEqual(example.output);
--- test-data/test-steps.js
+++ test-data/test-steps.js
@@ -1,22 +1,22 @@
-/*global module*/
-module.exports = function (ctx) {
+/*global module, defineStep, expect*/
+module.exports = function () {
 	'use strict';
-	ctx.defineStep(/Simple arithmetic: (\d*) plus (\d*) is (\d*)/, function (firstArg, secondArg, expectedResult) {
-		ctx.expect(firstArg + secondArg).toEqual(expectedResult);
+	defineStep(/Simple arithmetic: (\d*) plus (\d*) is (\d*)/, function (firstArg, secondArg, expectedResult) {
+		expect(firstArg + secondArg).toEqual(expectedResult);
 	});
-	ctx.defineStep(/Simple arithmetic: (\d*) and (\d*) added is (\d*) and multiplied is (\d*)/, function (firstArg, secondArg, expectedAdd, expectedMultiply) {
+	defineStep(/Simple arithmetic: (\d*) and (\d*) added is (\d*) and multiplied is (\d*)/, function (firstArg, secondArg, expectedAdd, expectedMultiply) {
 		this.assertEquals(expectedAdd, parseFloat(firstArg) + parseFloat(secondArg), 2);
 		this.assertEquals(expectedMultiply, parseFloat(firstArg) * parseFloat(secondArg), 3);
 	});
-	ctx.defineStep(/Multiple Assertions (\d*) is (\d*) and (.*)/, function (num1, num2, lineStatus) {
+	defineStep(/Multiple Assertions (\d*) is (\d*) and (.*)/, function (num1, num2, lineStatus) {
 		this.assertEquals(num2, num1, 1);
 		this.assertEquals(lineStatus, 'passes');
 	});
-	ctx.defineStep(/Multiple Assertions line ([a-z]*) and ([a-z]*)/, function (lineStatus1, lineStatus2) {
+	defineStep(/Multiple Assertions line ([a-z]*) and ([a-z]*)/, function (lineStatus1, lineStatus2) {
 		this.assertEquals(lineStatus1, 'passes');
 		this.assertEquals(lineStatus2, 'passes');
 	});
-	ctx.defineStep(/Star Wars has the following episodes:/, function (listOfEpisodes) {
+	defineStep(/Star Wars has the following episodes:/, function (listOfEpisodes) {
 		var episodes = [
 			'A New Hope',
 			'The Empire Strikes Back',
@@ -24,25 +24,25 @@ module.exports = function (ctx) {
 		this.assertSetEquals(listOfEpisodes.items, episodes);
 	});
 	var films = {}, tables = {};
-	ctx.defineStep(/These are the ([A-Za-z ]*) Films/, function (seriesName, tableOfReleases) {
+	defineStep(/These are the ([A-Za-z ]*) Films/, function (seriesName, tableOfReleases) {
 		films[seriesName] = tableOfReleases.items;
 		tables[seriesName] = tableOfReleases;
 	});
-	ctx.defineStep(/In total there a (\d*) ([A-Za-z ]*) Films/, function (numberOfFilms, seriesName) {
+	defineStep(/In total there a (\d*) ([A-Za-z ]*) Films/, function (numberOfFilms, seriesName) {
 		var actual = (films[seriesName] && films[seriesName].length) || 0;
 		this.assertEquals(parseFloat(numberOfFilms), actual, 0);
 	});
-	ctx.defineStep(/Good ([A-Za-z ]*) Films are/, function (seriesName, listOfEpisodes) {
+	defineStep(/Good ([A-Za-z ]*) Films are/, function (seriesName, listOfEpisodes) {
 		var actual = films[seriesName];
 		this.assertSetEquals(listOfEpisodes.items, actual);
 	});
-	ctx.defineStep(/Check ([A-Za-z ]*) Films/, function (seriesName, listOfEpisodes) {
+	defineStep(/Check ([A-Za-z ]*) Films/, function (seriesName, listOfEpisodes) {
 		this.assertUnorderedTableEquals(listOfEpisodes, tables[seriesName]);
 	});
-	ctx.defineStep(/List can contain sub lists/, function () {
+	defineStep(/List can contain sub lists/, function () {
 
 	});
-	ctx.defineStep(/\|([A-Za-z ]*) episode \| Year of release \|/, function (episode, yearOfRelease, seriesName) {
+	defineStep(/\|([A-Za-z ]*) episode \| Year of release \|/, function (episode, yearOfRelease, seriesName) {
 		var series = films[seriesName],
 			matching = series && series.filter(function (film) {
 				return film[0] === episode;
@@ -53,7 +53,7 @@ module.exports = function (ctx) {
 		this.assertEquals(yearOfRelease, actualYear, 1);
 	});
 
-	ctx.defineStep(/\| Positional Check episodes of ([A-Za-z ]*) \| Year of release \|/, function (episode, yearOfRelease, seriesName) {
+	defineStep(/\| Positional Check episodes of ([A-Za-z ]*) \| Year of release \|/, function (episode, yearOfRelease, seriesName) {
 		var series = films[seriesName],
 			matching = series && series.filter(function (film) {
 				return film[0] === episode;
