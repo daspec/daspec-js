17c67216fc76be10d65ab8871457237887979de0
Gojko Adzic <gojko@gojko.com>
2015-11-16 23:51:53 +0100

    linting

--- spec/example-blocks-spec.js
+++ spec/example-blocks-spec.js
@@ -6,12 +6,12 @@ describe('ExampleBlocks', function () {
 		var blocks = new ExampleBlocks('Line 1\nLine 2\nLine 3').getBlocks();
 		expect(blocks.map(function (block) {
 			return block.getMatchText();
-		})).toEqual([ ['Line 1'], ['Line 2'], ['Line 3'] ]);
+		})).toEqual([['Line 1'], ['Line 2'], ['Line 3']]);
 	});
 	it('should split lines delimited by CRLF', function () {
 		var blocks = new ExampleBlocks('Line 1\r\nLine 2\r\nLine 3').getBlocks();
 		expect(blocks.map(function (block) {
 			return block.getMatchText();
-		})).toEqual([ ['Line 1'], ['Line 2'], ['Line 3'] ]);
+		})).toEqual([['Line 1'], ['Line 2'], ['Line 3']]);
 	});
 });
